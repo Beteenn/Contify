aa65d4dd136a87156b559a3f14a7d28bfc0e8151
EnriqueAraujo <gilbertoaraujo229@gmail.com>
2020-06-04 14:24:31 -0300

    Criando moviment_files e linkando tabelas

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,4 +1,3 @@
-import 'dotenv/config';
 import express from 'express';
 import routes from './routes';
 
--- BackEnd/src/app/models/Moviment.js
+++ BackEnd/src/app/models/Moviment.js
@@ -20,6 +20,7 @@ class Moviment extends Model {
 
   static associate(models) {
     this.belongsTo(models.User, { foreignKey: 'user_id' });
+    this.belongsTo(models.MovimentFile, { foreignKey: 'picture_id' });
   }
 }
 
