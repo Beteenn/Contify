7e721cf9a00668dd89344fa36afe1372e7056f6f
Lucas Ramos Silva <lucas.silva@7comm.com.br>
2020-07-06 12:58:01 -0300

    category does no longer depends on user id

--- BackEnd/src/database/migrations/20200616035217-categories.js
+++ BackEnd/src/database/migrations/20200616035217-categories.js
@@ -7,13 +7,6 @@ module.exports = {
         primaryKey: true,
         autoIncrement: true,
       },
-      user_id: {
-        type: Sequelize.INTEGER,
-        references: { model: 'users', key: 'id' },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
-        allowNull: true,
-      },
       name: {
         type: Sequelize.STRING,
         allownull: false,
