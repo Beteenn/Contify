00b5aea82c9766fa894fdda0d1fb274c83f8ff66
Gilberto Barbosa <gilbertoaraujo229@gmail.com>
2020-12-20 13:51:09 -0300

    seeds separadas deletadas e criada seed master

---
+++ BackEnd/src/database/seeds/20201220024322-credit-companies.js
@@ -0,0 +1,398 @@
+module.exports = {
+  up: (queryInterface) => {
+    return queryInterface.bulkInsert(
+      'credit_companies',
+      [
+        {
+          name: 'Santander',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Visa',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'MasterCard',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'American Express',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Elo',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Itaú Unibanco S.A.',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Diners',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Aura',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Banco Inter',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Banco do Brasil',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Caixa',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Hipercard',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Itaú',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Neon',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Next',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'AgiBank',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Amazonia',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'BMG',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'BRB',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'BRDE',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'BS2',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Banco Cacique',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Banco PAN',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Banco Votorantin',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Banco do Nordeste',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Banese',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Banestes',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Banif',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Banpara',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Banrisul',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Bbm',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Cetelem',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Citibank',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Cruzeiro do Sul',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Daycoval',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Digi+',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Digio',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'FortBrasil',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'HSBC',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Mais!',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'N26',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Original',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Pag!',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'PagBank',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'PicPay',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Safra',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Sicoob',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Sicredi',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Sodexo',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Sofisa Direto',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Submarino',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Unicred',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Uniprime',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Viacredi',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'ABC Brasil',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Acesso Bank',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Americanas',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'C6 Bank',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Default',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Havan',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Mercantil do Brasil',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Meliuz',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Outros',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Porto Seguro',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Senff',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Shoptime',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Social Bank',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Superdigital',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Trigg',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Tudo Azul',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Urpay',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Vitreo',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Wirecard',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Woop',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Mercado Pago',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Renner',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'C&A',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ]
+    )
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.bulkDelete('credit_companies', null, {});
+  }
+};
