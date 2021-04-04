f6c2601264f28b58a4ac388b273a53df69c0c57e
fabio2804 <fabiovilela123@gmail.com>
2021-04-04 13:29:48 -0300

    refac(SplashPage): duration added into splash screen

--- Mobile/lib/app/view/starter/splash_page.dart
+++ Mobile/lib/app/view/starter/splash_page.dart
@@ -1,23 +1,31 @@
+import 'package:contify/app/view/authentication/login_page.dart';
 import 'package:contify/core/values/color.dart';
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: primaryColor,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: Image.asset(
-              'assets/images/contify_logo_branca.png',
-              width: 1000,
-            ),
-          ),
-        ],
+    return Stack(children: [
+      SplashScreen(
+        seconds: 3,
+        navigateAfterSeconds: LoginPage(),
       ),
-    );
+      Container(
+        color: primaryColor,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Image.asset(
+                'assets/images/contify_logo_branca.png',
+                width: 1000,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ]);
   }
 }
